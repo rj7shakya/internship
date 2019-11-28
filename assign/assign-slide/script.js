cbda3b5eaa19807b3a8f102651fd8868e2ae1f21
rj7shakya <shakyarajad1@gmail.com>
2019-11-28 08:30:12 +0545

    no oop

--- assign/assign-slide/script.js
+++ assign/assign-slide/script.js
@@ -1,43 +1,88 @@
 mainCont = document.getElementsByClassName("carousel-container");
 imgCont = document.getElementsByClassName("carousel-image-wrapper");
+imgCont[0].style.transition = 'left '+0.5+'s';
 
 var HEIGHT = 100;
 var WIDTH =100;
+var l= false;
 
-imgCont[0].style.width = WIDTH+'px';
-imgCont[0].style.height = HEIGHT+'px';
+function setWidth(slideIndex){
+    imgCont[0].style.width = WIDTH*slideIndex +'px';
+}
 
-var tag = document.getElementsByTagName('img')
-// var left = imgCont[0].style.left;
-var slideIndex=0;
-console.log(imgCont);
+var tag = document.getElementsByTagName('img');
+var slideIndex=1;
+
+// for a tag
+var left = document.createElement('a');
+left.innerHTML = '&#10094;';
+mainCont[0].appendChild(left);
+left.onclick =function(){
+    slide(-1);
+};
+left.style.cursor='pointer';
+left.style.position = 'absolute';
+left.style.left = 0+'px';
+left.style.top = 50+'%';
+
+var right = document.createElement('a');
+right.innerHTML = '&#10095;';
+mainCont[0].appendChild(right);
+right.onclick =function(){slide(1)};
+right.style.cursor='pointer';
+right.style.position = 'absolute';
+right.style.right = 0+'px';
+right.style.top = 50+'%';
+
+var indicat = document.createElement('div');
+indicat.className = 'ind';
+mainCont[0].appendChild(indicat);
+indicat.style.width = 100+'%';
+indicat.style.height = 15+'px';
+indicat.style.position ='absolute';
+indicat.style.bottom ='0px';
+indicat.style.textAlign = 'center';
 
 for(var i=0;i<tag.length;i++){
-    tag[i].style.position = 'relative';
-    tag[i].style.display = 'none';
-}
-change(0);
-function slidemove(n){
-    change(n);
+    tag[i].style.position = 'absolute';
+    tag[i].style.left = i*WIDTH +'px';
+    var dot = document.createElement('span');
+    dot.className='dot';
+    indicat.appendChild(dot); 
+    dot.style.cursor='pointer';
+    dot.style.height=10+'px';
+    dot.style.width=10+'px';
+    dot.style.margin=0+' 2'+'px';
+    dot.style.backgroundColor='red';
+    dot.style.borderRadius=50+'%';
+    dot.style.display='inline-block';
+    dot.style.onclick= function(){
+        slide(i+1);
+    };
+    dot.style.bottom=0+'px';
 }
 
 function change(n){
-    // imgCont[0].style.width = slideIndex *WIDTH +'px';
     slideIndex = n;
-    for(var i=0;i<tag.length;i++){
-        if(slideIndex == i){
-            tag[i].style.display = 'block';
-        }else{
-            tag[i].style.display = 'none';
-        }
-        console.log(slideIndex,i)
-    }
-    if(slideIndex < tag.length-1){
+    if(slideIndex < tag.length && slideIndex >0){
         slideIndex++;
-    }else{
-        slideIndex=0;
+    }else if(slideIndex >= tag.length){
+        slideIndex = 1;
+    }else if(slideIndex <= 1){
+        slideIndex =tag.length;
     }
-    setTimeout("change(slideIndex)",2000);
+
+    imgCont[0].style.left = -(slideIndex-1)*100 +'px';
 }
 
-// change(2);
+var auto =  setInterval(function(){change(slideIndex)},3000);
+var btn;
+function slide(n){
+    slideIndex+=n-1;
+    clearInterval(auto);
+    clearInterval(btn);
+    change(slideIndex);
+    btn= setInterval(function(){change(slideIndex)},3000);
+}
+change(1);
+
